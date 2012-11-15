f1fdf40341c43a0f48d79f70ed18b55104898b1c
mgawlista <mg@reqorder.com>
2012-11-15 14:34:49 +0100

    Undo last commit

--- spec/support/direct_uploader.rb
+++ spec/support/direct_uploader.rb
@@ -4,7 +4,5 @@ require File.dirname(__FILE__) << "/carrier_wave_config"
 
 class DirectUploader < CarrierWave::Uploader::Base
   include CarrierWaveDirect::Uploader
-  
-  store_dir '/store_dir/guid'
 end
 
