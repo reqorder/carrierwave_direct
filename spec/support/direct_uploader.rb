1efa460904cf23e037ced193e3ef9655c97a37e2
mgawlista <mg@reqorder.com>
2012-11-15 13:46:25 +0100

    Set store_dir

--- spec/support/direct_uploader.rb
+++ spec/support/direct_uploader.rb
@@ -4,5 +4,7 @@ require File.dirname(__FILE__) << "/carrier_wave_config"
 
 class DirectUploader < CarrierWave::Uploader::Base
   include CarrierWaveDirect::Uploader
+  
+  store_dir '/store_dir/guid'
 end
 
