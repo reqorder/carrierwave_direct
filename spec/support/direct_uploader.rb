c67ed3f92b13a7a12ba5a9e2a065e7a5b69b9bc4
mgawlista <mg@reqorder.com>
2012-11-15 15:48:45 +0100

    Defines default_url

--- spec/support/direct_uploader.rb
+++ spec/support/direct_uploader.rb
@@ -4,5 +4,9 @@ require File.dirname(__FILE__) << "/carrier_wave_config"
 
 class DirectUploader < CarrierWave::Uploader::Base
   include CarrierWaveDirect::Uploader
+  
+  def default_url
+    "/assets/default-avatar-#{version_name}.png"
+  end
 end
 
